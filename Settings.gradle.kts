025906aca2354834ed741847e584289ee1b6ed7c
tonytill1964-cpu <tonytill1964@gmail.com>
2026-08-11 17:14:27 +0100

    Initialize Settings.gradle.kts with repository settings

---
+++ Settings.gradle.kts
@@ -0,0 +1,16 @@
+settings.gradle.ktspluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+  
+rootProject.name = "Flip"
+include(":app")
